4f3a24379aea2247ca4694a846211564433b9763
Renato Galvão <renatopgalvao@gmail.com>
2023-09-15 13:12:08 -0300

    Projeto de uma rede social em MVC

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -2,19 +2,21 @@
 namespace src\controllers;
 
 use \core\Controller;
+use \src\handlers\LoginHandler;
 
 class HomeController extends Controller {
 
+    private $loggedUser;
+
+    public function __construct()
+    {
+        $loggedUser = LoginHandler::checkLogin();
+        if ($loggedUser === false) {
+            $this->redirect('/login');
+        }
+    }
+
     public function index() {
-        $this->render('home', ['nome' => 'Bonieky']);
+        $this->render('home',);
     }
-
-    public function sobre() {
-        $this->render('sobre');
-    }
-
-    public function sobreP($args) {
-        print_r($args);
-    }
-
 }
